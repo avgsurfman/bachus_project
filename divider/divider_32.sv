8d87354b7a087e0e815427e6234512b15b0b1f73
Franciszek Moszczuk <genericairconditionerno.169@gmail.com>
2025-11-26 17:40:57 +0100

    Working 32-bit unsigned divider!

--- divider/divider_32.sv
+++ divider/divider_32.sv
@@ -1,11 +1,13 @@
-// 32-bit multiplier. Calculates the difference for each row (d),
+// Unsigned 32-bit divider. Calculates the difference for each row (d),
 // then outputs the quotient and the remainder.
+
+`include "divider_block.sv"
 
 module divider_32(input logic [31:0] a,b,
                   output logic [31:0] rem, q);
       
       // can be parametrized
-      logic [31:0] couts [30:0];
+      logic [31:0] couts [31:0];
       //  D = R - B 
       //  D < 0 ? R' = R : R' = D 
       logic [31:0] difference;
@@ -16,35 +18,64 @@ module divider_32(input logic [31:0] a,b,
       // first layer is different as it has its inputs zeroed
           // bar except the last(first to be generated) column
           //divider_block(r, b, cin, n, cout, d, r_new);
-          begin: first_layer
-          divider_block layer0(a[31], ~b[0], 1'b1, difference[0], couts[0][0], , remainer[0][0];) // d unconnected
-          for (i = 1; i < 30; i++) 
-              //the rest of R inputs are pulled down to GND
-              //divider_block(r, b, cin, n, cout, d, r_new);
-              divider_block(1'b0, ~b[i], couts[0][i-1], difference[0], couts[0][i], , remainder[0][i]); // d unconnected
+          for (i = 0; i < 32; i++) begin: first_layer 
+              if (i == 0)
+                  divider_block layer0(
+                                      .r(a[31]), 
+                                      .b(~b[0]),
+                                      .cin(1'b1),
+                                      .n(difference[0]),
+                                      .cout(couts[0][0]),
+                                      .d(),
+                                      .r_new(remainder[0][0])
+                                      ); // d unconnected
+              else if (i == 31)
+                  divider_block layer0(.r(1'b0), 
+                                       .b(~b[31]), 
+                                       .cin(couts[0][30]), 
+                                       .n(difference[0]),
+                                       .cout(), 
+                                       .d(difference[0]),
+                                       .r_new()); // couts and r_new omitted
+              else
+                  //divider_block(r, b, cin, n, cout, d, r_new);
+                  //the rest of R inputs are pulled down to GND
+                  divider_block layer0(.r(1'b0),
+                                       .b(~b[i]), 
+                                       .cin(couts[0][i-1]), 
+                                       .n(difference[0]),
+                                       .cout(couts[0][i]),
+                                       .d(), 
+                                       .r_new(remainder[0][i])); // d unconnected
+          
           //divider_block(r, b, cin, n, cout, d, r_new);
-          divider_block layer0(a[0], ~b[31], couts[0][30], difference[0], , difference[0], ); // couts and r_new omitted
           end 
-          for (k = 1; k < 31; i++) begin: next_layer
-              divider_block(a[31-k], ~b[0], 1'b1, difference[k], couts[k][0], , remainder[k][0]);
-              for (i = 1; i < 30; i++)
-                  divider_block(remainder[k-1][i-1], ~b[i], couts[k][i-1], difference[k], couts[k][i], , remainder[k][i]);
-              divider_block(.r(remainder[k-1][30]), 
+          for (k = 1; k < 32; k++) begin: next_layer
+              for (i = 0; i < 32; i++) begin
+                  if (i == 0)
+                     divider_block layerk_first(a[31-k], ~b[0], 1'b1, difference[k], couts[k][0], , remainder[k][0]);
+                  else if (i == 31)
+                      divider_block layerk_last(
+                            .r(remainder[k-1][30]), 
                             .b(~b[31]), 
                             .cin(couts[k][i-1]), 
                             .n(difference[k]), 
                             .cout(),
                             .d(difference[k]), 
-                            .r_new()); //remainder[k][i]);
+                            .r_new(remainder[k][i])); 
+                  else 
+                     divider_block layerk(remainder[k-1][i-1], ~b[i], couts[k][i-1], difference[k], couts[k][i], , remainder[k][i]);
+          end
           end
       end
       endgenerate
 
 
      // quotient generate
+     //
      generate
-         for(k = 0, k < 31; k++)
-            assign q[k] = ~difference[k]; 
+         for(k = 0; k < 32; k++)
+            assign q[k] = ~difference[31-k]; 
      endgenerate
      // remainder
      assign rem[31:0] = remainder[31];
--- divider/divider_test.py
+++ divider/divider_test.py
@@ -5,7 +5,7 @@ import cocotb
 from cocotb.triggers import Timer
 
 @cocotb.test()
-async def multiplier_2times2_test(dut):
+async def divider_2dividedby2_test(dut):
     """Test for 2 / 2 = 1."""
 
     A = 2
@@ -18,13 +18,13 @@ async def multiplier_2times2_test(dut):
 
 
     assert dut.q.value == 1, (
-        f"Div result is incorrect: A={dut.a.value}, B={dut.b.value} {dut.y.value} != 4"
+        f"Div result is incorrect: A={dut.a.value}, B={dut.b.value} {dut.q.value} != 1"
     )
 
 
 @cocotb.test()
-async def multiplier_5dividedby10_test(dut):
-    """Test for 5 + 10"""
+async def divider_5dividedby10_test(dut):
+    """Test for 5 / 10"""
 
     A = 5
     B = 10
@@ -34,9 +34,11 @@ async def multiplier_5dividedby10_test(dut):
 
     await Timer(2, unit="ns")
 
-
+    assert dut.rem.value == 5, (
+        f"Div result is incorrect: Bad remainder. A={dut.a.value}, B={dut.b.value} {dut.rem.value} != {5}"
+    )
     assert dut.q.value == 0, (
-        f"Div result is incorrect: A={dut.a.value}, B={dut.b.value} {dut.y.value} != {0}"
+        f"Div result is incorrect: Bad quotient. A={dut.a.value}, B={dut.b.value} {dut.q.value} != {0}"
     )
 
 
@@ -62,7 +64,7 @@ async def multiplier_5dividedby10_test(dut):
 
 @cocotb.test()
 async def anything_dividedby1_test(dut):
-    """Multiplying by one."""
+    """Dividing by one."""
 
     A = random.randint(1, 2**(5));
     B = 1
@@ -74,7 +76,7 @@ async def anything_dividedby1_test(dut):
 
 
     assert dut.q.value == A, (
-        f"Critical Error: ANYTHING DIVIDED BY ONE IS NOT ITSELF! A={dut.a.value}, B={dut.b.value} {dut.y.value} != A"
+        f"Critical Error: ANYTHING DIVIDED BY ONE IS NOT ITSELF! A={dut.a.value}, B={dut.b.value} {dut.q.value} != {A}"
     )
 
 ## divide
