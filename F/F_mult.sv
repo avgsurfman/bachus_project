a5e3a6516161ce9f50780a75acc75dba296a4ada
Franciszek Moszczuk <genericairconditionerno.169@gmail.com>
2026-01-17 03:47:05 +0100

    F-mult framemwork

--- F/F_mult.sv
+++ F/F_mult.sv
@@ -3,26 +3,46 @@
 // In the future (so probably never)
 
 `include "../multiplier/unsigned/multiplier.sv"
+`include "../adder/hdl/sklansky_adder.sv"
 `include "./F_isNaN.sv"
 
 module F_mult (input logic [31:0] a, b,
+               input logic [2:0]  rounding,  
                input logic clk,
+               output logic flags [4:0], // NV DZ OF UF NX
                output logic [31:0] y);
 
-always_ff@(posedge clk)
-//
+               
+//always_ff@(posedge clk) register?
+
+logic isNanA, isInfA, isSubnormalA, isZeroA, isNormalA;
+logic isNanB, isInfB, isSubnormalB, isZeroB, isNormalB;
+
+F_isNaN        encoderA(A, isNaNA, isInfA, isSubnormalA, isZeroA, isNormalA);
+F_isNaN        encoderB(B, isNaNA, isInfB, isSubnormalB, isZeroB, isNormalB);
+
+
+//// Exponent
+
+/// CSA Adder
+// Adds expA + expB - 127 (bias)
+
+
+//// Mantissa, Significand
 
 logic [47:0] mul;
 
-/// Mult with implicit one
+/// Mult with implicit ones
 multiplier_bw_unsigned#(SIZE = 24)
                        (.a({1'b1, a[22:0]}), 
                         .b({1'b1, b[22:0]}),
                         .y(mul));
 
-/// Shift-due uses a priority encoder with casez? maybe?
+/// shiftDue uses a priority encoder with casez to shift the result until it's normalized? maybe?
 
+logic shiftDue [4:0]; 
 
+ 
 
 // need rounding based on FSCR
 endmodule
